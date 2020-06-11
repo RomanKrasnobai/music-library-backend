9bee9397954cf52687a80dd94a4f192107bcd087
roman.krasnobai <roman.krasnobai@ironsrc.com>
2020-06-11 14:51:37 +0300

    fix request from front

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { Express } from 'express';
+import { json } from 'body-parser';
+import { ExpressAdapter } from '@nestjs/platform-express';
+
+const server: Express = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   await app.listen(3000);
+
+  app.use(json({limit: '50mb'}));
+  app.init();
 }
+
 bootstrap();
